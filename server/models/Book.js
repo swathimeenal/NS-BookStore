e1ec6bd0cb6d6a3c90b0ed7ddf5e2dbf9c5e1fe6
swathimeenal <swathimeenal1994@gmail.com>
2024-03-08 01:58:14 +0530

    update

--- server/models/Book.js
+++ server/models/Book.js
@@ -4,6 +4,7 @@ const bookSchema = new mongoose.Schema({
     name: {type: String},
     author: {type: String, required: true},
     imageUrl: {type: String, required: true},
+    contentUrl: {type: String, required: true}
 })
 
 const bookModel = mongoose.model('Book', bookSchema)
