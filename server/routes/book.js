bd5245f20170ffd6d466c10e54d441a01f9de98b
swathimeenal <swathimeenal1994@gmail.com>
2024-05-03 13:03:09 +0530

    completed

--- server/routes/book.js
+++ server/routes/book.js
@@ -9,7 +9,10 @@ import { verifyAdmin } from "./auth.js";
 // multer setup
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/Users/nagarajanrajan/Desktop/Capstone Project/NS BookStore/client/src/uploads");
+    cb(
+      null,
+      "/Users/nagarajanrajan/Desktop/Capstone Project/NS BookStore/client/src/uploads"
+    );
   },
   filename: function (req, file, cb) {
     console.log(file);
@@ -18,16 +21,31 @@ const storage = multer.diskStorage({
   },
 });
 const PdfSchema = mongoose.model("Book");
-const upload = multer({ storage: storage ,limits: { fieldSize: 10 * 1024 * 1024 }});
+const upload = multer({
+  storage: storage,
+  limits: { fieldSize: 10 * 1024 * 1024 },
+});
+// get Route
+router.get("/",  (req, res) => {
+  try {
+    console.log(req);
+    res.status(200).send(` <h1><b>MERN STACK BOOK STORE APP</b></h1>`);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false
+    });
+  }
+});
 
 // Route for Handling file uploads
-router.post("/add",upload.single("file"), verifyAdmin, async (req, res) => {
+router.post("/add", upload.single("file"), verifyAdmin, async (req, res) => {
   const name = req.body.name;
   const author = req.body.author;
   const image = req.body.image;
   const pdfFile = req.body.pdf;
-  
-  console.log(pdfFile)
+
+  console.log(pdfFile);
   try {
     await PdfSchema.create({
       name: name,
@@ -48,31 +66,9 @@ router.post("/add",upload.single("file"), verifyAdmin, async (req, res) => {
   //  }
 });
 //
-router.get('/', (req,res)=>{
-  try {
-   console.log(req)
-  res.status(200).send(`
-  <h1><b>MERN STACK BOOK STORE APP</b></h1>
-  `)
-  } catch (error) {
-      console.log(error);
-      res.status(500).send({
-          message:error.message,
-          success:false
-      })
-  }
-})
-
 
 // Get All books
-router.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find();
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+
 
 router.get("/books", async (req, res) => {
   try {
@@ -96,7 +92,7 @@ router.get("/book/:id", async (req, res) => {
 router.put("/book/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    console.log(req.body)
+    console.log(req.body);
     const book = await Book.findByIdAndUpdate({ _id: id }, req.body);
     return res.json({ updated: true, book });
   } catch (err) {
